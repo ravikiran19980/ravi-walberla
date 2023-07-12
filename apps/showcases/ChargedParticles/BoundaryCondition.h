0e7e85ea7626517a1f0bc1c79a226ce45363aa12
zy69guqi <richard.angersbach@fau.de>
2023-07-12 12:29:30 +0200

    Make value type of BoundaryCondition.h a real_t

--- apps/showcases/ChargedParticles/BoundaryCondition.h
+++ apps/showcases/ChargedParticles/BoundaryCondition.h
@@ -19,18 +19,18 @@ struct BoundaryCondition
       return type_;
    }
 
-   double getValue() {
+   real_t getValue() {
       return value_;
    }
 
-   BoundaryCondition(stencil::Direction direction, std::string type, double value)
+   BoundaryCondition(stencil::Direction direction, std::string type, real_t value)
       : direction_(direction), type_(std::move(type)), value_(value)
    {}
 
  protected:
    stencil::Direction direction_;
    std::string type_;
-   double value_;
+   real_t value_;
 }; // the struct defined here needs to be used in different files,
 
 } // namespace walberla
