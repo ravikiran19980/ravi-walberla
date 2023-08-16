d2097e68322c152b490a0e2a4cfe3e72a29638e9
ravikiran <ravi.k.ayyala@fau.de>
2023-08-16 15:06:34 +0200

    Job clang-tidy #55175 fix

--- apps/showcases/ChargedParticles/ChargedParticles.cpp
+++ apps/showcases/ChargedParticles/ChargedParticles.cpp
@@ -830,7 +830,7 @@ int main(int argc, char** argv)
 
    case Showcase: {
       WALBERLA_LOG_INFO_ON_ROOT("Charged Particle Showcase Simulation is Chosen");
-      generationDomain = simulationDomain.createFromMinMaxCorner(0, 0, 0, 200, 200, 800);
+      generationDomain = math::GenericAABB<real_t>::createFromMinMaxCorner(0, 0, 0, 200, 200, 800);
       inflowVec        = Vector3< real_t >(0_r, 0_r, uInflow);
 
       uint_t particleCount = 0;
@@ -858,7 +858,7 @@ int main(int argc, char** argv)
 
       WALBERLA_LOG_INFO_ON_ROOT("Initiating User Defined Simulation");
       if (extendSimulationDomain) { generationDomain = simulationDomain.getExtended(-0.5_r * Spacing); }
-      else { generationDomain = simulationDomain.createFromMinMaxCorner(0, 0, 0, 200, 200, 800); }
+      else { generationDomain = math::GenericAABB<real_t>::createFromMinMaxCorner(0, 0, 0, 200, 200, 800);}
       inflowVec = Vector3< real_t >(0_r, 0_r, uInflow);
 
       for (auto pt : grid_generator::SCGrid(generationDomain, generationDomain.center(), Spacing))
