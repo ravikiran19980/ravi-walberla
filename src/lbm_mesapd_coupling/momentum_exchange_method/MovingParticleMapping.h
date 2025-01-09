80feb1da9a6a94f9bb3de7a34f31a740f65627b3
Samuel Kemmler <samuel.kemmler@fau.de>
2025-01-09 11:17:41 +0100

    Fix modernize-type-traits in lbm_mesapd_coupling

--- src/lbm_mesapd_coupling/momentum_exchange_method/MovingParticleMapping.h
+++ src/lbm_mesapd_coupling/momentum_exchange_method/MovingParticleMapping.h
@@ -59,7 +59,7 @@ namespace lbm_mesapd_coupling {
 template< typename PdfField_T, typename BoundaryHandling_T, typename ParticleAccessor_T, typename ParticleSelector_T>
 class MovingParticleMapping
 {
-   static_assert(std::is_base_of<mesa_pd::data::IAccessor, ParticleAccessor_T>::value, "Provide a valid accessor as template");
+   static_assert(std::is_base_of_v<mesa_pd::data::IAccessor, ParticleAccessor_T>, "Provide a valid accessor as template");
 
 public:
 
