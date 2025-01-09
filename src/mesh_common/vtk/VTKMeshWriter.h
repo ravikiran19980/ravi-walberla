d850974367aae1ffbba83362651195b75479d92a
Christoph Alt <christoph.alt@fau.de>
2025-01-09 15:53:53 +0100

    replaced a int introduced by clang-tidy with auto

--- src/mesh_common/vtk/VTKMeshWriter.h
+++ src/mesh_common/vtk/VTKMeshWriter.h
@@ -298,7 +298,7 @@ void VTKMeshWriter<MeshType>::writePiece( std::ostream & os ) const
 
    os << "        <DataArray type=\"Int32\" Name=\"offsets\" format=\"binary\">\n";
    os << "          ";
-   for(int  const& offset : offsets)
+   for(auto const& offset : offsets)
    {
       b64 << offset;
    }
