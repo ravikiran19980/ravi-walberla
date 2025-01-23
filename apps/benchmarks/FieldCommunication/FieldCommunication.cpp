b42a0006457da20096fd9873376db24ffacda70a
zy69guqi <richard.angersbach@fau.de>
2025-01-23 14:58:59 +0100

    Fix modernize-loop-convert in benchmarks/FieldCommunication

--- apps/benchmarks/FieldCommunication/FieldCommunication.cpp
+++ apps/benchmarks/FieldCommunication/FieldCommunication.cpp
@@ -354,8 +354,8 @@ int main( int argc, char **argv )
             auto databaseBlock = config->getBlock( "Database" );
             if ( databaseBlock )
             {
-                for ( auto it = databaseBlock.begin(); it != databaseBlock.end(); ++it )
-                    stringProperties[it->first] = it->second;
+                for (const auto & it : databaseBlock)
+                    stringProperties[it.first] = it.second;
             }
 
             realProperties["total_min"] = real_c( timingPool["totalTime"].min()) / real_c( iterations );
