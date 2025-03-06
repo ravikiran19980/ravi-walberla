3b73120eab98786428226f307ff133901b1480b2
dy94rovu <ravi.k.ayyala@fau.de>
2025-03-06 14:49:38 +0100

    Modernize - use default member initializations for normalizedDragOld_ and normalizedDragNew_

--- apps/benchmarks/FluidParticleCoupling/DragForceSphere.cpp
+++ apps/benchmarks/FluidParticleCoupling/DragForceSphere.cpp
@@ -209,8 +209,7 @@ public:
                        const shared_ptr<ParticleAccessor_T>& ac, walberla::id_t sphereID )
 :     timeloop_( timeloop ), setup_( setup ), blocks_( blocks ),
       flagFieldID_( flagFieldID ), pdfFieldID_( pdfFieldID ),
-      ac_( ac ), sphereID_( sphereID ),
-      normalizedDragOld_( 0.0 ), normalizedDragNew_( 0.0 )
+      ac_( ac ), sphereID_( sphereID )
    {
       // calculate the analytical drag force value based on the series expansion of chi
       // see also Sangani - Slow flow through a periodic array of spheres, IJMF 1982. Eq. 60 and Table 1
@@ -350,8 +349,8 @@ private:
    const walberla::id_t sphereID_;
 
    // drag coefficient
-   real_t normalizedDragOld_;
-   real_t normalizedDragNew_;
+   real_t normalizedDragOld_{ 0.0 };
+   real_t normalizedDragNew_{ 0.0 };
 
    real_t averageVel_;
 
