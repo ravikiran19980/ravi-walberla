09a6eca7066f37eaf6283be04d54be4c66ddfe3f
dy94rovu <ravi.k.ayyala@fau.de>
2025-10-17 09:18:19 +0200

    removed velocity mapping for psmSweepCollectionTemperature and only have it for psmSweepCollectionFluid and also pass only the particlevelocities field of psmSweepCollectionFluid into the psmenergy constructor

--- apps/benchmarks/MaterialTransport/thermal_PSM/thermal_PSM_Catalyst_Particles/thermalPSMCHT.cpp
+++ apps/benchmarks/MaterialTransport/thermal_PSM/thermal_PSM_Catalyst_Particles/thermalPSMCHT.cpp
@@ -825,7 +825,6 @@ int main(int argc, char** argv)
    for (auto blockIt = blocks->begin(); blockIt != blocks->end(); ++blockIt)
    {
       psmSweepCollectionFluid.setParticleVelocitiesSweep(&(*blockIt));
-      psmSweepCollectionTemperature.setParticleVelocitiesSweep(&(*blockIt));
       psmSweepCollectionTemperature.setParticleTemperaturesSweep(&(*blockIt)); // the initial temperatures of particles are always uniform
       initializeConcentrationField(&(*blockIt));
       pdfSetterFluid(&(*blockIt));
@@ -1001,7 +1000,7 @@ int main(int argc, char** argv)
 
 
    pystencils::PSMEnergySweep psmEnergySweep(
-      particleAndVolumeFractionSoA_energy.BsFieldID, particleAndVolumeFractionSoA_energy.BFieldID,densityConcentrationFieldCPUGPUID,particleAndVolumeFractionSoA_energy.particleVelocitiesFieldID,
+      particleAndVolumeFractionSoA_energy.BsFieldID, particleAndVolumeFractionSoA_energy.BFieldID,densityConcentrationFieldCPUGPUID,particleAndVolumeFractionSoA_fluid.particleVelocitiesFieldID,
       pdfFieldEnergyCPUGPUID,velFieldFluidCPUGPUID,  Cp_f,Cp_s,Qs,omegaT_f,omegaT_s,dummy_ref, densityFluid, densityParticle);
 
 
@@ -1032,8 +1031,6 @@ int main(int argc, char** argv)
    timeloop.add() << Sweep(deviceSyncWrapper(psmSweepCollectionFluid.setParticleVelocitiesSweep),
                            "Set particle velocities from fluid sweepcollection");
    timeloop.add() << Sweep(deviceSyncWrapper(psmSweepCollectionTemperature.particleMappingSweep), "Particle mapping Thermal"); // always uses a weighting of 1
-   timeloop.add() << Sweep(deviceSyncWrapper(psmSweepCollectionTemperature.setParticleVelocitiesSweep),
-                           "Set particle velocities from thermal sweepcollection");
 
    timeloop.add() << Sweep(deviceSyncWrapper(psmFluidSweep), "PSM Fluid sweep")
                   << AfterFunction(
