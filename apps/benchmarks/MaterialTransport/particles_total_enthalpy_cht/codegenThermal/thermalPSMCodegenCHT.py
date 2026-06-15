cb012ac76004970e13d66325ab9d899707a6f0a9
dy94rovu <ravi.k.ayyala@fau.de>
2026-06-15 10:19:33 +0200

    Revert "commented out clear cache which is problematic for builds at times"
    This reverts commit 141c667a509aaf7759773cc4df574909c0c967f4.

--- apps/benchmarks/MaterialTransport/particles_total_enthalpy_cht/codegenThermal/thermalPSMCodegenCHT.py
+++ apps/benchmarks/MaterialTransport/particles_total_enthalpy_cht/codegenThermal/thermalPSMCodegenCHT.py
@@ -34,7 +34,7 @@ from pystencils.cache import clear_cache
 from psmclass import ThermalPSMConfig,create_thermal_lb_method,create_psm_thermal_collision_rule
 from lbmpy.methods.creationfunctions import CollisionSpaceInfo
 from lbmpy.enums import Stencil, Method, CollisionSpace
-#clear_cache()
+clear_cache()
 
 info_header = """
 const char * infoStencil_fluid = "{stencil}";
